d629726bf6d01a8bb8138e1495dd45e9cdf3e4f5
David Chaparro <chaparrodavid91@gmail.com>
2021-01-07 19:31:01 -0400

    funcion crear

--- cargaGoogleSheets.py
+++ cargaGoogleSheets.py
@@ -5,13 +5,23 @@ from oauth2client.service_account import ServiceAccountCredentials
 def auth_google():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name('AutomarizacionTiendas-506d946c64f0.json',scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('AutomarizacionTiendas-fca572e81aca.json',scope)
 
     client = gspread.authorize(creds)
 
     sheet = client.open('data-prueba')
-
+    csv_to_sheets(sheet,client)
+def csv_to_sheets(sheet,client):
+    auth_google()
     with open('data.csv','r') as file_obj:
         content = file_obj.read()
-        client.import_csv(sheet.id, data=content)
+        res = client.import_csv(sheet.id, data=content)
+        return res
+
+def create_new_sheets(sheet,client):
+    auth_google()
+    sheet.add_worksheet(rows=1430,cols=2,title='Nombres1')
+    sheet_runs = sheet.get_worksheet(1)
+    
 auth_google()
+
--- productosTodoEsLed.py
+++ productosTodoEsLed.py
@@ -17,8 +17,5 @@ def consulta_sql():
 
     sql = ("SELECT  pstl_product.state as state,  pstl_product.id_product as idProduct,  pstl_product_attribute.id_product_attribute  as combinacion_id,pstl_product.reference as referencia, pstl_product_attribute.reference as referencia_combinacion,pstl_product.quantity as cantidad,pstl_product_attribute.quantity as cantidad_referencia,pstl_product.ean13 as ean13_producto,pstl_product_attribute.ean13 as ean13_combinacion,pstl_product.price as precio_producto,pstl_product_attribute.price as precio_combinacion,pstl_product.wholesale_price as precio_mayor, pstl_product_attribute.wholesale_price as precio_m_combinacion FROM pstl_product  LEFT JOIN pstl_product_attribute on pstl_product.id_product  = pstl_product_attribute.id_product_attribute UNION ALL SELECT  pstl_product.state as state,  pstl_product.id_product as idProduct,  pstl_product_attribute.id_product_attribute  as combinacion_id,pstl_product.reference as referencia, pstl_product_attribute.reference as referencia_combinacion,pstl_product.quantity as cantidad,pstl_product_attribute.quantity as cantidad_referencia,pstl_product.ean13 as ean13_producto,pstl_product_attribute.ean13 as ean13_combinacion,pstl_product.price as precio_producto,pstl_product_attribute.price as precio_combinacion,pstl_product.wholesale_price as precio_mayor, pstl_product_attribute.wholesale_price as precio_m_combinacion FROM pstl_product  RIGHT JOIN pstl_product_attribute on pstl_product.id_product  = pstl_product_attribute.id_product_attribute")
 #mycursor.execute(sql)
-    myresult = pd.read_sql_query(sql,mydb)
-
-    df = pd.DataFrame(myresult)
-    df.to_csv(r'data.csv',index=False)
+    convierte_a_csv(sql,mydb)
 consulta_sql()
